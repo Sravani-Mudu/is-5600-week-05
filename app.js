e32d2ad5d222ad0e920e406d3e52a7d967df9bd2
Sravani-Mudu <sravani.mudunuri@slu.edu>
2025-11-24 02:14:28 +0000

    changes 2

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 const api = require('./api')
 const middleware = require('./middleware')
-const bodyParser = require('body-parser')
 
+const app = express()
 
-// Set the port
-const port = process.env.PORT || 3000
-// Boot the app
-@@ -19,6 +17,11 @@ app.get('/products/:id', api.getProduct)
+// Middleware
+app.use(bodyParser.json())
+
+// Product routes
+app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
-// Boot the server
-app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+// Order routes
 app.get('/orders', api.listOrders)
 app.post('/orders', api.createOrder)
-app.put('/orders/:id', api.editOrder);
-app.delete('/orders/:id', api.deleteOrder); 
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
 
 // Boot the server
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server listening on port ${port}`))
