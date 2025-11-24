33b0ada3275b38d96fbd13c631f901fd930e3e19
Sravani-Mudu <sravani.mudunuri@slu.edu>
2025-11-24 02:02:49 +0000

    files changed

--- products.js
+++ products.js
@@ -1,14 +1,41 @@
 const fs = require('fs').promises
 const path = require('path')
+const cuid = require('cuid')
+const db = require('./db')
 
 const productsFile = path.join(__dirname, 'data/full-products.json')
+
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
 
 /**
  * List products
  * @param {*} options 
- * @returns 
- */
-async function list(options = {}) {
+@@ -12,38 +41,64 @@ async function list(options = {}) {
 
   const { offset = 0, limit = 25, tag } = options;
 
@@ -17,11 +44,26 @@ async function list(options = {}) {
     .filter(product => {
       if (!tag) {
         return product
+  
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
       }
+    }
+  } : {}
+
+  const products = await Product.find(query)
+    .sort(({_id: 1}))
+    .skip(offset)
+    .limit(limit)
+
+  return products
 
       return product.tags.find(({ title }) => title == tag)
     })
     .slice(offset, offset + limit) // Slice the products
+    
 }
 
 /**
@@ -38,12 +80,42 @@ async function get(id) {
       return products[i]
     }
   }
+async function get(_id) {
+  const product = await Product.findById(_id)
+  return product
+}
+
+
+async function create (fields) {
+  const product = await new Product(fields).save()
+
+  return product
+}
+
+async function edit (_id, change) {
+  const product = await get(_id)
+
+  Object.keys(change).forEach(function(key) {
+    product[key] = change[key]
+  })
+
+  await product.save()
+
+  return product
+}
+
 
   // If no product is found, return null
   return null;
+async function destroy (_id) {
+  return await Product.deleteOne({_id})
 }
 
 module.exports = {
   list,
   get
+  get,
+  create,
+  edit,
+  destroy,
 }
